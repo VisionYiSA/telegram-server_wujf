60e75bab392cfa99e9415a7829d749e07507ba76
Yuichi Hagio <yhagio87@gmail.com>
2014-06-08 08:33:09 +0900

    WIP: trying to connect with client router

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var app = express('express');
+
+var User = require('../telegram-cli/app/models/user')
+
+app.get('/', function(req, res){
+	User.find(function(err, users){
+		if(err) res.send(err);
+		res.json(users);
+	});
+});
+app.get('/login');
+app.get('/resetpassword');
+app.get('/sentpassnotify');
+
+app.get('/posts');
+app.get('/users/:user_id');
+app.get('/users/:user_id/following');
+app.get('/users/:user_id/followers');
+
 var server = app.listen(3000, function(){
 	console.log('Listening on port %d', server.address().port);
 });
