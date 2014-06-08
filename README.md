f74013d988042b58968fe03acfef9f54c606a361
Yuichi Hagio <yhagio87@gmail.com>
2014-06-08 19:57:46 +0900

    WIP: Loading fixture from telegram-cli folder

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 * Client side repo: [https://github.com/yhagio/telegram-cli](https://github.com/yhagio/telegram-cli)
 
 #### Todos:
-* implement all the routes required by the ember.js app with the same dummy data from the previously used fixtures adapter (client side)
+* Implement all the routes required by the ember.js app with the same dummy data from the previously used fixtures adapter (client side). 
+[Routing ](http://expressjs.com/4x/api.html#app.VERB), [req.params ](http://expressjs.com/4x/api.html#req.params), [res.send()](http://expressjs.com/4x/api.html#res.send)
--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 var express = require('express');
 var app = express('express');
 
-var User = require('../telegram-cli/app/models/user')
+var User = require('../telegram-cli/app/models/user');
+var Post = require('../telegram-cli/app/models/post');
 
+// All Routes
 app.get('/', function(req, res){
-	User.find(function(err, users){
-		if(err) res.send(err);
-		res.json(users);
-	});
-});
-app.get('/login');
-app.get('/resetpassword');
-app.get('/sentpassnotify');
 
-app.get('/posts');
-app.get('/users/:user_id');
-app.get('/users/:user_id/following');
-app.get('/users/:user_id/followers');
+});
+app.get('/login', function(req, res){
+
+});
+app.get('/resetpassword', function(req, res){
+	
+});
+app.get('/sentpassnotify', function(req, res){
+	
+});
+
+app.get('/posts', function(req, res){
+	
+});
+
+app.get('/users/:user_id', function(req, res){
+
+});
+app.get('/users/:user_id/following', function(req, res){
+	
+});
+app.get('/users/:user_id/followers', function(req, res){
+	
+});
 
 var server = app.listen(3000, function(){
 	console.log('Listening on port %d', server.address().port);
