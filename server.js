00467d9a5b9ac8703ee4fa2df66a48c54e1398f4
Yuichi Hagio <yhagio87@gmail.com>
2014-06-11 14:43:25 +0900

    WIP: Routing, removed html from server.js and some array data, and using bodyParser

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+app.use(bodyParser());
 
 var Post = [
 	{
@@ -15,16 +15,6 @@ var Post = [
     body: 'Salut! This is 2nd oldest post, from Joseph!',
     user: 'joseph',
     date: 'Sat, 10 May 2014 21:33:30 GMT'
-  },{
-    id: 3,
-    body: 'Hey everyone, I am Gal Gagot!',
-    user: 'gal',
-    date: 'Tue, 20 May 2014 22:33:32 GMT'
-  },{
-    id: 4,
-    body: 'What up! This is latest post!',
-    user: 'beyonce',
-    date: 'Thu, 29 May 2014 23:33:30 GMT'
   }];
 
 var User = [{
@@ -39,35 +29,14 @@ var User = [{
     email: 'beyonce@example.com',
     password: 'password',
     avatar: ''
-  },{
-    id: 'joseph',
-    name: 'Joseph Gordon-Levitt',
-    email: 'joseph@example.com',
-    password: 'password',
-    avatar: ''
-  },{
-    id: 'gal',
-    name: 'Gal Gagot',
-    email: 'gal@example.com',
-    password: 'password',
-    avatar: ''
   }];
 
 // All Routes
 app.get('/', function(req, res){
-	res.send("\
-		<form method='post' action='/'>\
-			<input type='text' name='email' placeholder='email'/>\
-			<input type='text' name='name' placeholder='name'/>\
-			<input type='text' name='username' placeholder='username'/>\
-			<input type='password' name='password' placeholder='password'/>\
-			<input type='submit' value='Submit' />\
-		</form>\
-	");
+	res.send('Register');
 });
+
 app.post('/', function(req, res){
-	console.log(req.body);
-	console.log(JSON.stringify(req.body));
 	var userInfo = {			
 		id: req.body.username,
 		name: req.body.name,
@@ -76,33 +45,23 @@ app.post('/', function(req, res){
 		avatar: ''
 	};
 	User.push(userInfo);
-	res.send(200, {user: userInfo});
-	// res.redirect('/posts');
+	res.redirect('/login');
 })
+
 app.get('/login', function(req, res){
-	res.send("\
-		<form method='post' action='/login'>\
-			<input type='text' name='username' placeholder='username'/>\
-			<input type='password' name='password' placeholder='password'/>\
-			<input type='submit' value='Login' />\
-		</form>\
-	");
+	res.send('Login');
 });
+
 app.post('/login', function(req, res){
 	var username = req.body.username;
 	var password = req.body.password;
-	var theUser = {
-		id: 'will',
-    name: 'Will Smith',
-    email: 'will@example.com',
-    password: 'password',
-    avatar: ''};
-	if(username == theUser.username && password == theUser.password){
+	if(username === User[User.length-1].id && password === User[User.length-1].password){
 		res.redirect('/posts');
 	} else {
 		res.send('User credential does not match.');
 	}
 });
+
 app.get('/resetpassword', function(req, res){
 	
 });
@@ -114,30 +73,27 @@ app.get('/sentpassnotify', function(req, res){
 });
 
 app.get('/posts', function(req, res){
-	res.send("\
-		<form method='post' action='/posts'>\
-			<input type='text' name='body' placeholder='body'/>\
-			<input type='submit' value='Publish' />\
-		</form>\
-		<div>Body: "+Post[Post.length-1].body+"</div>\
-	");
-	// res.send(200, {posts: Post});
+	res.send(200, {posts: Post});
 });
+
 app.post('/posts', function(req, res){
 	Post.push({ 
 		body: req.body.body,
-		user: 'CurrentUser',
-		date: 'A few seconds ago'
+		user: User[User.length-1].id,
+		date: new Date()
 	});
 	res.send(200, {posts: Post});
+	res.redirect('/posts')
 });
 
 app.get('/users/:user_id', function(req, res){
 	res.send(200, {user: req.params.user_id});
 });
+
 app.get('/users/:user_id/following', function(req, res){
 	res.send(200, {user: req.params.user_id});
 });
+
 app.get('/users/:user_id/followers', function(req, res){
 	res.send(200, {user: req.params.user_id});
 });
